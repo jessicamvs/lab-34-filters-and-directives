f6111f1e3b62913e4817c8bc7bc6021f11dadaa1
Jessica Vasquez-Soltero <jmichellevs@gmail.com>
2017-03-12 12:29:06 -0700

    switch to template string

--- lab-jessica/app/filter/fuzzy-search.js
+++ lab-jessica/app/filter/fuzzy-search.js
@@ -4,7 +4,7 @@ module.exports = function() {
   return function(galleries, str){
     if(!str) return galleries;
 
-    let pattern = '.*' + str.toUpperCase().split('').join('.*') + '.*';
+    let pattern = `.*${str.toUpperCase().split('').join('.*')}.*`;
     let regExp = new RegExp(pattern);
 
     return galleries.filter(gallery => regExp.test(gallery.name.toUpperCase()));
